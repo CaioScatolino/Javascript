a798e6f5f9003b7b0b1a924d13aad7ebd0bd2d06
Caio Scatolino <caio.scatolino@outlook.com>
2022-11-04 18:35:12 -0300

    Refazendo as aulas de Javascript

---
+++ aulas-iniciais-JS/script.js
@@ -0,0 +1,14 @@
+// Exercício 1
+
+let carro = "Ferrari"
+console.log(carro);
+
+// Exercício 2
+
+let bolo = {"tipo" : "chocolate", "preço" : 1.99}
+console.log(bolo);
+
+// Exercício 3
+
+let cidade = "São Paulo"
+console.log (cidade)
