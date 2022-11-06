af183ec19297de6e0eac7f16f213bc23ce609d8b
Caio Scatolino <caio.scatolino@outlook.com>
2022-11-06 14:23:45 -0300

    Adicionando aulas de if, else if, else

---
+++ if-else/script.js
@@ -0,0 +1,19 @@
+let idade = 90
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+
+if (idade >= 18 && idade < 60) {
+    console.log("Você é um adulto")
+}
+
+else if (idade < 18) {
+    console.log("Vocé uma criança")
+}
+
+else if (idade >= 60) {
+    console.log("Você é um idoso")
+}
