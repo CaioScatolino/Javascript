6e1a122f65fc6f1b9049846e88e976be9348eb78
Caio Scatolino <caio.scatolino@outlook.com>
2022-11-07 17:48:54 -0300

    Acrescentando mais algumas aulas

---
+++ condicional-ternario/script.js
@@ -0,0 +1,12 @@
+let isMember = true
+
+let shipping = isMember ? 2 : 10
+console.log(`O Frete é de: ${shipping}`)
+
+console.log(`O membro é: ${isMember ? "premium" : "não membro"}`)
+
+
+let age = 90
+
+let isAdult = age >= 18 ? "Maior de idade" : "Menor de idade"
+console.log(isAdult)
---
+++ if-else-exercicio-2/script.js
@@ -0,0 +1,31 @@
+// Ex 1
+
+let x = 10;
+let y = 5
+
+console.log(x > y)
+
+// Ex 2
+
+let w = "10"
+
+let z = 10
+
+console.log(w === z)
+
+// Ex 3
+
+let a = 10
+let b = 5
+
+console.log(a != 5)
+
+// Ex 4
+
+let preco = 40.3
+
+if (preco <= 45) {
+    console.log("Está barato")
+} else {
+    console.log("Está caro")
+}
