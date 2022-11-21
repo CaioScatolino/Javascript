feb0d5c82e1e00f7e2f22776fa7fb449eaa8bbc9
Caio Scatolino <caio.scatolino@outlook.com>
2022-11-21 17:53:01 -0300

    organizando pastas e mais aulas

---
+++ loop/loop-exercicios/script.js
@@ -0,0 +1,15 @@
+// 1 - Mostrar todas as frutas com loop
+
+let fruits = ['Maçã', 'Uva', 'Banana']
+
+for (fruit of fruits) {
+    console.log(fruit)
+}
+
+// 2 - Conte de 1 até 100 através de um while
+
+let i = 0
+while(i <= 100) {
+    console.log(i)
+    i++
+}
---
+++ loop/loop-while/script.js
@@ -0,0 +1,10 @@
+let numero = 0
+
+while( numero < 10 ) {
+    console.log(`O número da vez é ${numero}`)
+    numero++
+}
+
+while (true) {
+    
+}
