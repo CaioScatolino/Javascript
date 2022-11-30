ab5020260e3c2028aa68897aa63b8539cb5272ff
Caio Scatolino <caio.scatolino@outlook.com>
2022-11-30 17:07:11 -0300

    Adicionando mais aulas

---
+++ DOM/eventos-de-clique/script.js
@@ -0,0 +1,7 @@
+function clicou() {
+    alert("Você clicou")
+}
+
+let botao = document.querySelector('.botao2')
+
+botao.addEventListener("click", clicou)
---
+++ DOM/selecionando-elementos-1 copy/script.js
@@ -0,0 +1,5 @@
+// document.getElementById("teste").innerHTML = "Bla 2"
+
+// document.querySelector("#teste").innerHTML = "Bla 3"
+
+document.querySelectorAll("#teste ul li")
