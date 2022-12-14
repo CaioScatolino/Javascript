e508478afbf6a4971cd1a080de9918028722c2fd
Caio Scatolino <caio.scatolino@outlook.com>
2022-12-14 18:35:50 -0300

    Adicionando mais aulas de Jquery

---
+++ jquery/manipulando-atributos/script.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $('a').attr('href', 'https://google.com.br')
+
+    $('img').attr('width', '200').attr('height', '100')
+
+    $('#teste button').addClass('estilo').html('Novo Nome')
+    
+})
---
+++ jquery/selecionar-elementos-html/script.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.lista2 li.item')
+
+    if($('#botao').length > 0) {
+        alert("Tem botao na pagina")
+    }
+})
