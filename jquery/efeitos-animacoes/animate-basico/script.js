a315b387605f5214a93535a844b9fe234385ad00
Caio Scatolino <caio.scatolino@outlook.com>
2023-01-07 10:51:01 -0300

    Adicionando mais aulas de Jquery

---
+++ jquery/efeitos-animacoes/animate-basico/script.js
@@ -0,0 +1,14 @@
+$(function() {
+
+$('.botao').bind('click', function(){
+    $('.div').animate({
+        'margin-left': 100,
+        'margin-top': 20,
+        'width': 500,
+        'border-radius': 75
+    }, 1000)
+})
+
+
+
+})
