a97e729e838a94dc6c5b6e09d641265378b4eb39
Caio Scatolino <caio.scatolino@outlook.com>
2023-02-01 17:52:33 -0300

    Adicionando mais aulas

---
+++ requisicoes/callback/script.js
@@ -0,0 +1,9 @@
+// O que é um Callback?
+
+// call back = ligar de volta
+
+function alerta() {
+    alert("Clicou no botao")
+}
+
+$('#botao').bind('click', alerta)
---
+++ requisicoes/requisicao-na-pratica/script.js
@@ -0,0 +1,16 @@
+// https://jsonplaceholder.typicode.com/posts
+
+function clicou() {
+    
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+            return response.json()
+        }) 
+        .then((json) => {
+
+            alert(`O título do primeiro post é: ${json[0].title}`)
+        })
+
+}
+
+$('#botao').bind('click', clicou)
