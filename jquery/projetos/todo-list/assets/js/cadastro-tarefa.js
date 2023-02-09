8abc7f55437039ff56faa388fa5f071138c01922
Caio Scatolino <caio.scatolino@outlook.com>
2023-02-09 18:17:51 -0300

    Adicionando mais aulas

--- jquery/projetos/todo-list/assets/js/cadastro-tarefa.js
+++ jquery/projetos/todo-list/assets/js/cadastro-tarefa.js
@@ -1,7 +1,63 @@
-$(function() {
+$(function () {
 
 
-    console.log("Teste")
 
 
+    $('#form-cadastro-tarefa').submit(function (e) {
+
+        let tarefa = $("#tarefa").val()
+        let data = $("#data").val()
+
+        if (!tarefa || !data) {
+
+            $('#alert-cadastro-tarefa-erro').show()
+            return false
+        }
+
+        salvaTarefa(tarefa, data)
+
+        e.preventDefault()
+    })
+
+    $('.btn-close').bind('click', function () {
+
+
+        $(this).parent().css("display", "none")
+
+    })
+
+
+
+    $('#data').mask('00/00/0000')
 })
+
+function salvaTarefa(tarefa, data) {
+
+    const objetoTarefaParaSalvar = { id: new Date().getTime(), tarefa, data }
+
+    let tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas-todo-list'))
+
+
+    if (tarefasCadastradas && tarefasCadastradas.length) {
+
+        tarefasCadastradas.push(objetoTarefaParaSalvar)
+
+        localStorage.setItem('tarefas-todo-list',
+            JSON.stringify(tarefasCadastradas)
+        )
+    } else {
+
+        localStorage.setItem('tarefas-todo-list',
+            JSON.stringify([objetoTarefaParaSalvar])
+        )
+    }
+
+
+    $('#alert-cadastro-tarefa-sucesso').show()
+
+    $('#tarefa').val('')
+    $('#data').val('')
+
+}
+
+
