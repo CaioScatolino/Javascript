9b0932c3925dd4d777073ba36a2ef7398a185b1d
Caio Scatolino <caio.scatolino@outlook.com>
2023-02-13 18:01:52 -0300

    Adicionando mais aulas

--- OO/projeto-luta/script.js
+++ OO/projeto-luta/script.js
@@ -1,4 +1,12 @@
 let char = new Sorcerer('Caio')
 
-console.log(char.name)
-console.log(char.life)
+let monster = new LittleMonster()
+
+const stage = new Stage(
+    char, 
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
+
+stage.start()
