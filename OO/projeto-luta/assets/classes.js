9e9a6f86ccabd0efd2c0e2783913fecd97d8bd0c
Caio Scatolino <caio.scatolino@outlook.com>
2023-02-17 18:20:57 -0300

    Atualizando aulas

--- OO/projeto-luta/assets/classes.js
+++ OO/projeto-luta/assets/classes.js
@@ -91,19 +91,37 @@ class Stage {
 
     update() {
         // Fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life}/${this.fighter1.maxLife} HP`
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)}/${this.fighter1.maxLife.toFixed(1)} HP`
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100
         this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
 
         // Fighter 2
-        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life}/${this.fighter2.maxLife} HP`
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)}/${this.fighter2.maxLife.toFixed(1)} HP`
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
     }
 
     doAttack(attacking, attacked) {
 
-        console.log(`${attacking.name} está atacando ${attacked.name}`)
+        if(attacking.life <= 0 || attacked.life <= 0) {
 
+            console.log("Atacando cachorro morto")
+            return;
+        }
+
+        let attackFactor = (Math.random() * 2)
+        let defenseFactor = (Math.random() * 2)
+
+        let actualAttack = (attacking.attack * attackFactor).toFixed(2)
+        let actualDefense = (attacked.defense * defenseFactor).toFixed(2)
+
+        if(actualAttack > actualDefense) {
+            attacked.life -= (actualAttack-(attacked.defense*0.2))
+            console.log(`${attacking.name} causou ${actualAttack} de dano em ${attacked.name}`)
+        }else {
+            console.log(`${attacked.name} conseguiu defender o dano`)
+        }
+
+        this.update()
     }
 }
